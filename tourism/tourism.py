bbce8c45edaa21473686c420fbda2b2ebc79be12
yctsaiyc <yctsaiyc@gmail.com>
2024-12-25 17:39:24 +0800

    feat(tourism): (WIP)

--- tourism/tourism.py
+++ tourism/tourism.py
@@ -3,6 +3,7 @@ import requests
 from urllib.parse import unquote
 from bs4 import BeautifulSoup
 import pandas as pd
+import re
 
 
 class Tourism:
@@ -20,6 +21,21 @@ class Tourism:
 
         self.checkpoint_path = checkpoint_path
         self.checkpoint = self.get_checkpoint()
+
+        self.columns = [
+            "類型",
+            "觀光風景區",
+            "觀光遊憩區",
+            "縣市",
+            "年月",
+            "遊客人次",
+            "去年同月遊客人次",
+            "差值",
+            "成長率(%)",
+            "遊客人次計算方式",
+            "遊客人次計算類型",
+            "資料來源",
+        ]
 
     def get_checkpoint(self):
 
@@ -111,7 +127,7 @@ class Tourism:
             df = self.process_df(df)
 
             # 存檔
-            csv_path = os.path.join(self.data_dir, xlsx_file.replace(".xlsx", "_2.csv"))
+            csv_path = os.path.join(self.data_dir, xlsx_file.replace(".xlsx", ".csv"))
             df.to_csv(csv_path, index=False)
             print(f"Saved {csv_path}.")
 
@@ -120,23 +136,33 @@ class Tourism:
         # 刪除欄位名稱的換行符號
         df.columns = [col.replace("\n", "") for col in df.columns]
 
+        # 重新命名部份欄位名稱
+        df = df.rename(
+            columns={
+                "類型Type": "類型",
+                "觀光遊憩區Scenic Spots": "觀光遊憩區",
+                "縣市City/Country": "縣市",
+                "上年同月遊客人次": "去年同月遊客人次",
+            }
+        )
+
         # 刪除英文
-        cols = ["類型Type", "觀光遊憩區Scenic Spots", "縣市City/Country"]
+        cols = ["類型", "觀光遊憩區", "縣市"]
         df[cols] = df[cols].map(lambda x: x.split("\n")[0] if isinstance(x, str) else x)
 
         # 新增「觀光風景區」欄位
         del_idx = []
 
         for idx, row in df.iterrows():
-            if row["類型Type"] not in ["國家公園", "國家級風景特定區"]:
+            if row["類型"] not in ["國家公園", "國家級風景特定區"]:
                 break
 
-            if pd.isna(row["縣市City/Country"]):
-                park = row["觀光遊憩區Scenic Spots"]
+            if pd.isna(row["縣市"]):
+                park = row["觀光遊憩區"]
                 del_idx.append(idx)
                 continue
 
-            elif row["觀光遊憩區Scenic Spots"].startswith(" "):
+            elif row["觀光遊憩區"].startswith(" "):
                 df.at[idx, "觀光風景區"] = park
 
         df = df.map(lambda x: x.replace("  ", "") if isinstance(x, str) else x)
@@ -151,10 +177,25 @@ class Tourism:
         # 刪除資料來源備註
         df = df[:-2]
 
-        # 新增遊客人數計算類型欄位
-        df["遊客人數計算類型"] = df["遊客人次計算方式"].map(
+        # 新增遊客人次計算類型欄位
+        df["遊客人次計算類型"] = df["遊客人次計算方式"].map(
             lambda x: self.get_count_type(x)
         )
+
+        # 替換成長率的"-"為空值
+        df["成長率(%)"] = df["成長率(%)"].replace("-", "")
+
+        # 新增年月欄位
+        pattern = re.compile(r"^\d{2,3}年\d{1,2}月遊客人次$")
+
+        for col in df.columns:
+            if pattern.match(col):
+                df["年月"] = col.replace("遊客人次", "")
+                df = df.rename(columns={col: "遊客人次"})
+                break
+
+        # 排序欄位
+        df = df[self.columns]
 
         return df
 
